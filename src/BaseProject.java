015ca8d27bc0b5ff6c6fadb331f98978d4ee558f
Milo <florianbulling@gmail.com>
2021-10-04 14:02:01 +0200

    Initial commit

---
+++ src/BaseProject.java
@@ -0,0 +1,9 @@
+public class BaseProject {
+
+    // ~ "main" + [tab] => program entry function
+    public static void main(String[] args) {
+        // ~ "sout" + [tab] => system console output function
+        System.out.println("Hello, World!");
+    }
+
+}
