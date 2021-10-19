b3cccf7b0ad233314d1f34078dd4e500c8b71b80
Milo <florianbulling@gmail.com>
2021-10-19 15:47:05 +0200

    Create JavaImportsExample.java

---
+++ y21-w42/src/JavaImportsExample.java
@@ -0,0 +1,9 @@
+import JavaImportsExample.ExampleModule;
+
+public class JavaImportsExample {
+    public static void main(String[] args) {
+        ExampleModule myModule = new ExampleModule();
+
+        myModule.printOut();
+    }
+}
