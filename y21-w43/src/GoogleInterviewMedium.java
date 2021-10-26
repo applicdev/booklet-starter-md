5b60ab3dca9f5890b77e908fa2fc3248ff0104c3
Milo <florianbulling@gmail.com>
2021-10-26 11:44:37 +0200

    Update GoogleInterviewMedium.java

--- y21-w43/src/GoogleInterviewMedium.java
+++ y21-w43/src/GoogleInterviewMedium.java
@@ -1,50 +1,88 @@
 public class GoogleInterviewMedium {
     public static void main(String[] args) {
         int[][] input = {
-                {1, 0, 0, 0, 1, 0, 1},
-                {1, 0, 1, 1, 0, 1, 0},
-                {0, 1, 0, 1, 1, 0, 0},
-                {0, 0, 1, 0, 1, 0, 0},
-                {1, 1, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 1, 0, 0, 0, 1, 1, 0, 0},
+                {0, 1, 0, 0, 0, 1, 1, 0, 0},
+                {0, 0, 1, 0, 1, 1, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 0, 1, 0},
+                {1, 1, 0, 1, 0, 1, 0, 1, 1},
+                {0, 1, 1, 1, 0, 0, 0, 0, 0},
+        };
+        int[][] inputLarge = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
         };
 
+        // ~ bruteforce solution
+        interviewBFSolution(input);
+        interviewBFSolution(inputLarge);
+
+        // ~
+        interviewSearchSolution(input);
+        interviewSearchSolution(inputLarge);
+    }
+
+    public static void printGrid(String type, int[][] value) {
+        System.out.println("\n" + type + ": ");
+        for (int n = 0; n < value.length; n++)
+            for (int m = 0; m < value[n].length; m++)
+                System.out.print("" + (value[n][m] == 1 ? "X" : " ") + (m < value[n].length - 1 ? " " : "\n"));
+
+    }
+
+    public static void interviewSearchSolution(int[][] input) {
         int[][] result = new int[input.length][input[0].length];
         int[][] filter = new int[input.length][input[0].length];
 
-        for (int d = 0; d < Math.max(input.length, input[0].length) - 1; d++) {
+        // ~
+    }
+
+    public static void interviewBFSolution(int[][] input) {
+        int[][] result = new int[input.length][input[0].length];
+        int[][] filter = new int[input.length][input[0].length];
+
+        boolean matchedSomthing = true;
+        while (matchedSomthing) {
+            matchedSomthing = false;
+
             for (int n = 0; n < input.length; n++) {
                 for (int m = 0; m < input[n].length; m++) {
                     boolean matched = false;
 
+                    // ~
                     if (n - 1 < 0 || result[n - 1][m] == 1) matched = true;
                     if (m - 1 < 0 || result[n][m - 1] == 1) matched = true;
                     if (n + 1 > input.length - 1 || result[n + 1][m] == 1) matched = true;
                     if (m + 1 > input[n].length - 1 || result[n][m + 1] == 1) matched = true;
 
-                    if (matched) {
+                    if (matched && result[n][m] != input[n][m]) {
+                        matchedSomthing = true;
+
+                        // ~
                         result[n][m] = input[n][m];
                         filter[n][m] = 0;
-                    } else {
+                    } else if (!matched) {
+                        // ~
                         filter[n][m] = input[n][m];
                     }
                 }
             }
-
-            // ---
-            System.out.println("\ninput: ");
-            for (int n = 0; n < input.length; n++)
-                for (int m = 0; m < input[n].length; m++)
-                    System.out.print("" + input[n][m] + (m < input[n].length - 1 ? " " : "\n"));
-
-            System.out.println("\nresult: ");
-            for (int n = 0; n < result.length; n++)
-                for (int m = 0; m < result[n].length; m++)
-                    System.out.print("" + result[n][m] + (m < result[n].length - 1 ? " " : "\n"));
-
-            System.out.println("\nfilter: ");
-            for (int n = 0; n < filter.length; n++)
-                for (int m = 0; m < filter[n].length; m++)
-                    System.out.print("" + filter[n][m] + (m < filter[n].length - 1 ? " " : "\n"));
-            // ---
         }
+
+        // ---
+        printGrid("input", input);
+        printGrid("result", result);
+        printGrid("filter", filter);
+        // ---
     }
+}
