1abc18311d8d6d2bf9fd8457e101fc05f203a977
Milo <florianbulling@gmail.com>
2021-10-31 14:15:43 +0100

    y21-w43

--- y21-w43/src/Assignments_Q1.java
+++ y21-w43/src/Assignments_Q1.java
@@ -33,26 +33,31 @@ public class Assignments_Q1 {
         System.out.println(result == null
                 ? "Coefficient too large; Input smaller numbers~\n"
                 : "result: " + ran + " choose " + cho + " = " + result + "\n");
+
+        // ~
+        System.out.println(result == null ? ""
+                : "Given a lottery with " + ran + " possible numbers and " + cho
+                + " to choose from, the possibility to match all " + cho + " is 1:" + result + ".\n");
     }
 
     private static java.lang.Long assignments_q1_binomialCoefficient(int r, int k) {
         // Concept:
-        // ~ Finding the binomial coefficient as code
+        // ~ Finding a function
         //
         //         10! = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
         //          7! =              7 * 6 * 5 * 4 * 3 * 2 * 1
         //     (10-7)! =                              3 * 2 * 1
         //
-        // 10 choose 7 =      10!                                     | " huge number "
-        //               --------------                               |  -------------   =  "int says no :)"
-        //                7! * (10-7)!                                |   huge number
+        // 10 choose 7 =      10!                                     |  issue: n! gets huge fast
+        //               --------------                               |  int  max. n! -> 12!
+        //                7! * (10-7)!                                |  long max. n! -> 20!
         //
         //             =   10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1     |
         //               -------------------------------------------  | "7 * 6 * 5 * 4 * 3 * 2 * 1" cancel out
         //                (7 * 6 * 5 * 4 * 3 * 2 * 1) * (3 * 2 * 1)   |
         //
         //             =   10 * 9 * 8                                 |
-        //               -------------------------------------------  | can be rewritten to prevent huge numbers
+        //               -------------------------------------------  | can be rewritten to prevent large numbers
         //                                               3 * 2 * 1    |
         //
         //             =  (10 / 1) * (9 / 2) * 8 / 3                  | looks loopable; something... something... --n, ++m
@@ -60,7 +65,7 @@ public class Assignments_Q1 {
         //             =  120
         //
         //
-        // ~ Inverse 'k' values have the same coefficient
+        // ~ Inverse 'k' values share the same coefficient
         //
         //         10! = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
         //          3! =                              3 * 2 * 1
@@ -77,13 +82,16 @@ public class Assignments_Q1 {
         // a.e.
         // 10 choose 7 == 120; requires 7 steps
         // 10 choose 3 == 120; requires 3 steps
-        if (k > r * 0.5) k = r - k;
+        if (k > r * 0.5F) k = r - k;
 
         // ~ Calculate the binomial coefficient of (r choose k)
         // a.e.
-        // 10 choose 10 ==   1 == (((((((((1 * 10 / 1) * 9 / 2) * 8 / 3) * 7 / 4) * 6 / 5) * 5 / 6) * 4 / 7) * 3 / 8) * 2 / 9) * 1 / 10
-        // 10 choose  6 == 210 == (((((1 * 10 / 1) * 9 / 2) * 8 / 3) * 7 / 4) * 6 / 5) * 5 / 6
-        java.lang.Long result = 1L;
+        // 10 choose 10 =   1
+        //              = (((((((((1 * 10 / 1) * 9 / 2) * 8 / 3) * 7 / 4) * 6 / 5) * 5 / 6) * 4 / 7) * 3 / 8) * 2 / 9) * 1 / 10
+        //
+        // 10 choose  6 = 210
+        //              = (((((1 * 10 / 1) * 9 / 2) * 8 / 3) * 7 / 4) * 6 / 5) * 5 / 6
+        long result = 1L;
         for (int n = r, m = 1; m <= k; --n, ++m) {
             result = result * n / m;
             if (result < 0) return null; // err; integer overflow
