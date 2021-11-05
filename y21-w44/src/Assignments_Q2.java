6ad50d7e2644d43ae21ebc7dd42e2209bfe148d4
Milo <florianbulling@gmail.com>
2021-11-05 23:38:43 +0100

    Update Assignments_Q2.java

--- y21-w44/src/Assignments_Q2.java
+++ y21-w44/src/Assignments_Q2.java
@@ -16,26 +16,26 @@ public class Assignments_Q2 {
         // - ...                    ...            ...
         // -                        1-10           divisible by 10
         // ---
-//        while (true) assignments_q2();
-        while (true) assignments_q2_b();
+        while (true) assignments_q2_a();
+//        while (true) assignments_q2_b();
     }
 
-    public static String assignments_q2_doubleToString(Double n) {
+    public static String assignments_q2_a_doubleToString(Double n) {
         return n == null ? "" : new BigDecimal(n).toPlainString();
     }
 
-    public static Double assignments_q2_stringToDouble(String n) {
+    public static Double assignments_q2_a_stringToDouble(String n) {
         return n.equals("") ? 0d : Double.parseDouble(n);
     }
 
-    public static Double assignments_q2_traverse(int range, Double value, int stack) {
+    public static Double assignments_q2_a_traverse(int range, Double value, int stack) {
         stack += 1;
 
         // ~
         for (int n = value == null ? 1 : 0; n <= 9; ++n) {
             // ~ Concatenate 'value' and 'i' -> 12_3 = 123
-            String plainValue = assignments_q2_doubleToString(value);
-            Double joindValue = assignments_q2_stringToDouble(plainValue + n);
+            String plainValue = assignments_q2_a_doubleToString(value);
+            Double joindValue = assignments_q2_a_stringToDouble(plainValue + n);
 
             if (joindValue % stack == 0) {
                 System.out.println("range:" + range + "\tstack: " + stack + "\tfound: " + plainValue + "_" + n);
@@ -43,7 +43,7 @@ public class Assignments_Q2 {
                 // ~ Return if done or concatenate the next digit
                 Double childValue = stack > range - 1
                         ? joindValue
-                        : assignments_q2_traverse(range, joindValue, stack);
+                        : assignments_q2_a_traverse(range, joindValue, stack);
 
                 if (childValue != null) return childValue; // Search has resolved~
             }
@@ -52,19 +52,19 @@ public class Assignments_Q2 {
         return null;
     }
 
-    public static void assignments_q2() {
+    public static void assignments_q2_a() {
         // ---
         System.out.println("\nEnter an integer as the length of the magic number:");
         int digits = new java.util.Scanner(System.in).nextInt();
-        int range = assignments_q2_doubleToString(Math.pow(10, digits - 1)).length(); // = digits;
+        int range = assignments_q2_a_doubleToString(Math.pow(10, digits - 1)).length(); // = digits;
 
 //        for (int digits = 1; digits < 50; ++digits) {
 //            double range = assignments_q2_doubleToString(Math.pow(10, digits) - 1).length();
         // ---
 
         // ~ Find the thing
-        Double result = assignments_q2_traverse(range, null, 0);
-        String plainResult = assignments_q2_doubleToString(result);
+        Double result = assignments_q2_a_traverse(range, null, 0);
+        String plainResult = assignments_q2_a_doubleToString(result);
 
         // f(4) = _1 mod 1 = 0
         //         |
